74b2eba8d01e02d9e2dab6c201ebcac8a6284b54
knooki <knukknakiknak@gmail.com>
2022-10-29 01:24:48 +0300

    added Event model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
   validates :name, :description, presence: true
+  has_one :event
 end
 #Здесть можно писать валадаторы
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_28_191958) do
+ActiveRecord::Schema.define(version: 2022_10_28_193446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,15 @@ ActiveRecord::Schema.define(version: 2022_10_28_191958) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "events", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "date", null: false
+    t.text "description"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_events_on_category_id"
+  end
+
+  add_foreign_key "events", "categories"
 end
